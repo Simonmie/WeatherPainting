4781c5c7fbf19e258d2c9826aeb8d3fa4061ab4e
Simonmie <16605913446@163.com>
2025-05-03 22:59:00 +0800

    feat: 调整项目结构

---
+++ src/contents/weatherIconMap.js
@@ -0,0 +1,31 @@
+// 天气代码映射到SVG图标
+export const weatherIconMap = {
+  0: "天气-晴.svg", // Clear sky
+  1: "天气-晴.svg", // Mainly clear
+  2: "天气-多云.svg", // Partly cloudy
+  3: "天气-阴天.svg", // Overcast
+  45: "天气-雾.svg", // Fog
+  48: "天气-霜.svg", // Depositing rime fog
+  51: "天气-小雨.svg", // Light drizzle
+  53: "天气-小雨.svg", // Moderate drizzle
+  55: "天气-中雨.svg", // Dense drizzle
+  56: "天气-雨加雪.svg", // Light freezing drizzle
+  57: "天气-雨加雪.svg", // Dense freezing drizzle
+  61: "天气-小雨.svg", // Slight rain
+  63: "天气-中雨.svg", // Moderate rain
+  65: "天气-大雨.svg", // Heavy rain
+  66: "天气-雨加雪.svg", // Light freezing rain
+  67: "天气-雨加雪.svg", // Heavy freezing rain
+  71: "天气-小雪.svg", // Slight snow fall
+  73: "天气-中雪.svg", // Moderate snow fall
+  75: "天气-大雪.svg", // Heavy snow fall
+  77: "天气-雪转晴.svg", // Snow grains
+  80: "天气-小雨.svg", // Slight rain showers
+  81: "天气-中雨.svg", // Moderate rain showers
+  82: "天气-暴雨.svg", // Violent rain showers
+  85: "天气-小雪.svg", // Slight snow showers
+  86: "天气-大雪.svg", // Heavy snow showers
+  95: "天气-雷雨.svg", // Thunderstorm
+  96: "天气-雷雨.svg", // Thunderstorm with slight hail
+  99: "天气-冰雹.svg", // Thunderstorm with heavy hail
+};
